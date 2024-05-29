31521baf8f24fc2c192d5fd4c2afc5544559d406
AnaReginaJP <158497896+AnaReginaJP@users.noreply.github.com>
2024-05-28 21:47:04 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,72 +1,65 @@
+<?php
+function calcula_paralaje(){
+    $distancia = 2.47e18; // Convertimos la distancia de kilómetros a metros (2.47E15 km * 1000 m/km * 1E3 para convertir a metros)
+    $resultado = 1.50e11 / $distancia;
+    return $resultado;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es-mx">
 <head>
     <meta charset="UTF-8">
-    <title>IntroPhp7 Grupo X__</title>
-    <link rel="stylesheet" href="css/estilos.css"/>
-    <link rel="stylesheet" href="css/menu.css"/>
-    <link rel="stylesheet" href="css/problemas.css"/>
-    <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
+    <title>Ejercicio Evaluación U3</title>
+    <link rel="stylesheet" href="css/stem.css"/>
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
 <body>
-   <section class="wrapper">
+  <section class="wrapper">
     <header>
-      <h1 class="logo"><a href="index.html">IntroPHP7</a></h1>
-      <nav>
-      <ul>
-        <li><a href="index.php" class="current">Inicio</a></li>
-        <li><a href=galaxia.php>Galaxia</a></li>
-      </ul>
-      </nav>
+      <h1 class="logo"><a href="stem.html">Evaluación</a></h1>
     </header>
-<section id="contenedor">
-<section  class="problema">
-  <h2>Problema: Cálcular la densidad de la Tierra</h2>
-  <p>Descripción:</p>
-  <p>El radio de la Tierra es 6 378 km y su masa es 5.97E24 kg. <br>
-a) ¿Cuál es la densidad promedio de la Tierra?<br>
-b) Si el radio de la Tierra se redujera a una tercera parte y su masa no cambiará, cuál serı́a su densidad?<br>
-c) Si el radio de la Tierra y su masa se redujera a una
-tercera parte, ¿su densidad serı́a la misma? ¿por qué?</p>
-</section>
-<section class="formulas">
-<h2>Fórmulas</h2>
-    Densidad = Masa/Volumen<br>
-    Volumen=(4/3)pi*r*r*r
-</section>
-<section class="datos">
-<h2>Datos:</h2>
-   m= 5.97E24 Kg. <br>
-   r= 6378 Km.
-</section>
-<section class="calculos">
-<h2>Solución</h2>
-   <p>a)La densidad de la Tierra es:<br>
-    densidad = 5.97E24 Kg/volumen (m) <br>
-    volumen= (4/3)*pi*(6378000)*(6378000)*(6378000)</p>
-</section>
-<?php
-     function calcula_densidad(){
-        $PI=3.141592;
-        $masa= 5.97E24;
-        $radio= 6378e3;
-        $volumen= (4/3)*$PI*$radio*$radio*$radio;
-        $densidad= $masa/$volumen;
-        return $densidad;
-     }
-?>
-<section class="resultado">
-<h2>Resultado:</h2>
-<div id="resultadoA"></div>
-<?php
- print "<h1> resultado: densidad = ".calcula_densidad(). " kg/(metro cubico)</h1>";
-?>
-</section>
-</section>
+    <section id="contenedor">
+      <section  class="problema">
+        <h2>¿Cuáles son los ángulos de paralaje? de una estrella cuya distancia respectivas a
+          la Tierra es la siguiente: </h2>
+        <p>2,47E15 km</p>
+      </section>
+      <section  class="esquemaProblema">
+        <h2>Esquema</h2>
+        <center>
+        <img class="imgProblema" src="images/esquema.jpeg">
+        </center>
+      </section>
+      <section class="formulas">
+        <h2>Fórmulas</h2>
+        d= d x 10^13<br>
+        p= 1,50 x 10 ^11 / d
+      </section>
+      <section class="datos">
+        <h2>Datos:</h2>
+          d= 2,47E15 km <br>
+          d en metros= 2,47 x 10^18 m<br>
+          1 rad= 57.2958 grados 
+      </section>
+      <section class="calculos">
+        <h2>Solución</h2>
+        <p>a)El paralelaje de los angulos de la estrella es:<br>
+        paralelaje, p(rad)= ((1,50 x 10^11)/ (2,47 x 10^18))</p>
+        <button onclick="calcula();">Presiona para calcular</button>
+      </section>
+      <section class="resultado">
+        <h2>Resultado:</h2>
+        <div id="resultadoA">
+            <?php
+            $paralaje = calcula_paralaje();
+            echo "El ángulo de paralaje de la estrella es aproximadamente de " . number_format($paralaje, 5, '.', '') . " radianes o " . number_format(rad2deg($paralaje), 4, '.', '') . " grados.";
+            ?>
+        </div>
+      </section>
+    </section>
     <footer class="pie">
-     Creative Commons versión 3.0 SciSoft 2024
     </footer>
-   </section>
+  </section>
 </body>
 </html>
